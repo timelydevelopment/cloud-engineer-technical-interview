28994e137dd712988ced01b54af928679b147fef
Sanil Lad <sanil@gettimely.com>
2022-08-02 20:34:39 +1200

    updated keyvault rbac policy

--- terraform/modules/keyvault/main-keyvault.tf
+++ terraform/modules/keyvault/main-keyvault.tf
@@ -5,6 +5,7 @@ resource "azurerm_key_vault" "vault" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 
   sku_name                        = "standard"
   enabled_for_template_deployment = true
